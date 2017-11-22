3987fc1261a3896dd2ed4fa4fce3989f5e5077ac
Jonathan Luk <jluk87@gmail.com>
2017-11-22 19:41:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string_to_reverse)
   new_string = ""
-  string_to_reverse.each do {|letter| new_string << string_to_reverse[-1..0]}
+  string_to_reverse.each {|letter| new_string << string_to_reverse[-1..0]}
   new_string
 end
 
