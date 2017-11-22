9ca3da6b7fe08176cfd525d37b7e3a57b556c59b
Jonathan Luk <jluk87@gmail.com>
2017-11-22 20:09:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 require 'pry'
 def reverse_each_word(string_to_reverse)
   new_string = ""
-  string_to_reverse.split(",").each do |letter|
-    new_string.insert(0, letter)
-    binding.pry
-  end
+  string_to_reverse.split('').each do {|letter| new_string.insert(0, letter)}
   puts new_string
 end
 
