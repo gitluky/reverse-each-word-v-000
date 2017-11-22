74b73f04d81716921a90654d5844952c46383051
Jonathan Luk <jluk87@gmail.com>
2017-11-22 19:52:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string_to_reverse)
   new_string = ""
-  string_to_reverse.split(",").each {|letter| new_string.insert(0, letter)}
+  string_to_reverse.split(",").each binging.pry {|letter| new_string.insert(0, letter)}
   puts new_string
 end
 
