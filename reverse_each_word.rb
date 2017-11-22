0f9ec519d164a1257d40eed27f068fe020cb44fd
Jonathan Luk <jluk87@gmail.com>
2017-11-22 19:45:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string_to_reverse)
   new_string = ""
   string_to_reverse.split(",").each {|letter| new_string.insert(0, letter) }
-  new_string
+  puts new_string
 end
 
 reverse_each_word("peanut")
