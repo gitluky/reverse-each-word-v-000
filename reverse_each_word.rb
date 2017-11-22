cb9407b308190ad449c1ab0763bc5b372bcad9ec
Jonathan Luk <jluk87@gmail.com>
2017-11-22 20:14:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,17 @@
+=begin
 def reverse_each_word(string_to_reverse)
   new_string = ""
   string_to_reverse.split('').each {|letter| new_string.insert(0, letter)}
   new_string
 end
+=end
+
+def reverse_each_word(string_to_reverse)
+  new_string = ""
+  string_to_reverse.split('').each {|letter| new_string.insert(0, letter)}
+  new_string
+end
+
+
 
 reverse_each_word("peanut")
